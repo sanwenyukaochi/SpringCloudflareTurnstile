046320906bf607663ac700061b6de8ca88cab95e
sanwenyukaochi <sanwenyukaochi@outlook.com>
2026-05-11 00:11:31 +0800

    feat: 新增 TurnstileConfiguration

---
+++ src/main/java/com/salmonspark/cloudflare/turnstile/TurnstileConfiguration.java
@@ -0,0 +1,32 @@
+package com.salmonspark.cloudflare.turnstile;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.health.autoconfigure.contributor.ConditionalOnEnabledHealthIndicator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Configuration
+@AutoConfiguration
+@Import({})
+public class TurnstileConfiguration {
+
+    @Configuration
+    @ConditionalOnClass(name = "io.micrometer.core.instrument.MeterRegistry")
+    @Import({})
+    static class TurnstileMetricsConfiguration {}
+
+    @Configuration
+    @ConditionalOnEnabledHealthIndicator("turnstile")
+    @ConditionalOnClass(name = "org.springframework.boot.health.contributor.HealthIndicator")
+    @Import({})
+    static class TurnstileHealthConfiguration {}
+
+    @PostConstruct
+    public void onStartup() {
+        log.info("SalmonSpark Spring Cloudflare Turnstile Service loaded");
+    }
+}
